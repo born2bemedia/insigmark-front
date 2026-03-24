8f31b5a4248e4fb0f2854b604b8ffcb90f60708c
SergWebAcceptance <serg.web@acceptance.ltd>
2026-03-24 10:12:19 +0200

    Update default locale in routing configuration from 'en' to 'pl' for improved localization support.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,5 +5,5 @@ export const routing = defineRouting({
   locales: ['en', 'de', 'it', 'pl'],
   localePrefix: 'as-needed',
   // Used when no locale matches
-  defaultLocale: 'en',
+  defaultLocale: 'pl',
 });
